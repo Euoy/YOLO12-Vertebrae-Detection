b58045621ac4617900c6e45e44602a806c286370
Euoy <65022422+Euoy@users.noreply.github.com>
2025-03-06 23:59:48 +0800

    c2 done and c7 pre-process not good

--- angle.py
+++ angle.py
@@ -11,8 +11,6 @@ c7_img_path = "E:\\YOLO11-Vertebrae-Detection"
 original_data_path = "E:\\bone_dataset"
 c2_coords_csv_path = "E:\\YOLO11-Vertebrae-Detection\\results\\c2.csv"
 c7_coords_csv_path = "E:\\YOLO11-Vertebrae-Detection\\results\\c7.csv"
-c2_canny_threshold = [100, 150]
-c7_canny_threshold = [100, 150]
 NUMBER_OF_DATASETS = 5
 """
 Data Constants
@@ -28,16 +26,13 @@ def line_eq(X, x1, y1, x2, y2):
 
     return m * (X - x1) + y1
 
-def edge_detection(img, thresholds):
+def edge_detection(img):
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sharped_img = cv2.equalizeHist(gray)
-    binarized_img = img_binarized(sharped_img, np.min(sharped_img) + 150)
-    gussian_img = cv2.GaussianBlur(binarized_img, (5, 5), 0)
-    edges = cv2.Canny(gussian_img, thresholds[0], thresholds[1])
-    cv2.imshow("bin", binarized_img)
-    # cv2.imshow("img", gray)
-    # cv2.waitKey(0)
+    sharped_img = hist_strech(gray)
+    sharped_img = cv2.equalizeHist(sharped_img)
+    edges = img_binarized(sharped_img, np.min(sharped_img) + 150)
+    cv2.imshow("bin", sharped_img)
 
     return edges
 
@@ -93,7 +88,7 @@ def coords_translate(points_to_transfer: list, original_img_number, c):
         
         return final_c7_coords
 
-def find_c2_bottom_points() -> list:
+def find_c2_bottom_points(img) -> list:
 
     left_half_x = []
     left_half_y = []
@@ -102,24 +97,32 @@ def find_c2_bottom_points() -> list:
 
     # split the points of edges to left half and right half
     for i in range(len(c2_coords)):
-        if(c2_coords[i][0] < c2_img_width / 2):
+        if (isbetween(c2_coords[i][0], (c2_img_width / 2) * c2_left_x_factor[0], (c2_img_width / 2) * c2_left_x_factor[1]) and isbetween(c2_coords[i][1], c2_img_height * c2_left_y_factor[0], c2_img_height * c2_left_y_factor[1])):
             left_half_x.append(c2_coords[i][0])
             left_half_y.append(c2_coords[i][1])
-        elif(c2_coords[i][0] > c2_img_width / 2 and (c2_coords[i][1] <= c2_img_height * right_half_ty_factor and c2_coords[i][1] >= c2_img_height * right_half_by_factor)):
+        elif (isbetween(c2_coords[i][0], (c2_img_width / 2) * (1 + c2_right_x_factor[0]), (c2_img_width / 2) * (1 + c2_right_x_factor[1])) and isbetween(c2_coords[i][1], c2_img_height * c2_right_y_factor[0], c2_img_height * c2_right_y_factor[1])):
             right_half_x.append(c2_coords[i][0])
             right_half_y.append(c2_coords[i][1])
+
+    # for i in range(len(right_half_x)):
+    #     cv2.circle(img, (right_half_x[i], right_half_y[i]), 1, (0, 0, 255), -1)
+    # for i in range(len(left_half_x)):
+    #     cv2.circle(img, (left_half_x[i], left_half_y[i]), 1, (0, 255, 0), -1)
+
+    # cv2.imshow("img", img)
+    # cv2.waitKey(0)
 
     # find the bottom left and right points
     index = np.where(left_half_y == np.max(left_half_y))[0][0]
     bottom_left_y = left_half_y[index]
     bottom_left_x = left_half_x[index]
-    index = np.where(right_half_x == np.max(right_half_x))[0][0]
+    index = np.where(right_half_y == np.max(right_half_y))[0][0]
     bottom_right_y = right_half_y[index]
     bottom_right_x = right_half_x[index]
 
     return [[bottom_left_x, bottom_left_y], [bottom_right_x, bottom_right_y]]
 
-def find_c7_top_points() -> list:
+def find_c7_top_points(img) -> list:
 
     left_half_x = []
     left_half_y = []
@@ -128,18 +131,26 @@ def find_c7_top_points() -> list:
 
     # split the points of edges to left half and right half
     for i in range(len(c7_coords)):
-        if(c7_coords[i][0] < c7_img_width / 2):
+        if (isbetween(c7_coords[i][0], (c7_img_width / 2) * c7_left_x_factor[0], (c7_img_width / 2) * c7_left_x_factor[1]) and isbetween(c7_coords[i][1], c7_img_height * c7_left_y_factor[0], c7_img_height * c7_left_y_factor[1])):
             left_half_x.append(c7_coords[i][0])
             left_half_y.append(c7_coords[i][1])
-        elif (c7_coords[i][0] > c7_img_width / 2):
-            right_half_x.append(c7_coords[i][0]) 
+        elif (isbetween(c7_coords[i][0], (c7_img_width / 2) * (1 + c7_right_x_factor[0]), (c7_img_width / 2) * (1 + c7_right_x_factor[1])) and isbetween(c7_coords[i][1], c7_img_height * c7_right_y_factor[0], c7_img_height * c7_right_y_factor[1])):
+            right_half_x.append(c7_coords[i][0])
             right_half_y.append(c7_coords[i][1])
+
+    # for i in range(len(right_half_x)):
+    #     cv2.circle(img, (right_half_x[i], right_half_y[i]), 1, (0, 0, 255), -1)
+    # for i in range(len(left_half_x)):
+    #     cv2.circle(img, (left_half_x[i], left_half_y[i]), 1, (0, 255, 0), -1)
+
+    # cv2.imshow("img", img)
+    # cv2.waitKey(0)
 
     # find the top left and right points
     index = np.where(left_half_y == np.min(left_half_y))[0][0]
     top_left_y = left_half_y[index]
     top_left_x = left_half_x[index]
-    index = np.where(right_half_x == np.max(right_half_x))[0][0]
+    index = np.where(right_half_y == np.min(right_half_y))[0][0]
     top_right_y = right_half_y[index]
     top_right_x = right_half_x[index]
 
@@ -161,11 +172,23 @@ def get_vetical_vector(vector):
 def get_angle(vector1, vector2):
     return np.arccos(np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)))
 
-def img_binarized(img, threshold1, threshold2 = 255):
+def img_binarized(img, threshold1):
 
-    _, binary = cv2.threshold(img, threshold1, threshold2, cv2.THRESH_BINARY)
+    _, binary = cv2.threshold(img, threshold1, 255, cv2.THRESH_BINARY)
 
     return binary
+
+def isbetween(x, a, b) -> bool:
+    return a <= x <= b
+
+def hist_strech(img):
+
+    img_max = np.max(img)
+    img_min = np.min(img)
+    cdf = (img - img_min) * (255 / (img_max - img_min))
+    cdf = cdf.astype(np.uint8)
+
+    return cdf
 
 """
 Function defs
@@ -179,10 +202,15 @@ if __name__ == "__main__":
 
     original_c2_coords = pandas.read_csv(c2_coords_csv_path)
     original_c7_coords = pandas.read_csv(c7_coords_csv_path)
-    right_half_ty_factor = 0.9
-    right_half_by_factor = 0.75
-    c2_x_offset = 20
-    c7_x_offset = 20
+    c2_right_y_factor = [0.7, 0.9]
+    c2_left_y_factor = [0.8, 0.95]
+    c2_right_x_factor = [0.2, 0.6]
+    c2_left_x_factor = [0.1, 0.8]
+    c7_right_y_factor = [0.1, 0.3]
+    c7_left_y_factor = [0.2, 0.4]
+    c7_right_x_factor = [0, 1]
+    c7_left_x_factor = [0, 1]
+
     """
     Initialization
     """
@@ -199,38 +227,24 @@ if __name__ == "__main__":
         c7_img_width = c7_img.shape[1]
         c7_img_height = c7_img.shape[0]
 
-        c2_edges = edge_detection(c2_img, c2_canny_threshold)
+        c2_edges = edge_detection(c2_img)
         c2_indices = np.where(c2_edges != [0])
         c2_coords = np.column_stack((c2_indices[1], c2_indices[0])).tolist()
 
-        c7_edges = edge_detection(c7_img, c7_canny_threshold)
+        c7_edges = edge_detection(c7_img)
         c7_indices = np.where(c7_edges != [0])
         c7_coords = np.column_stack((c7_indices[1], c7_indices[0])).tolist()
 
-        # find the points to remove from detection
-        c2_index_to_pop = []
-        for i in range(len(c2_coords)):
-            if c2_coords[i][0] > c2_img_width - c2_x_offset:
-                c2_index_to_pop.append(i)
-        for i in range(len(c2_index_to_pop)):
-            c2_coords.pop(c2_index_to_pop[i] - i)
+        c2_edges = cv2.cvtColor(c2_edges, cv2.COLOR_GRAY2BGR)
+        c7_edges = cv2.cvtColor(c7_edges, cv2.COLOR_GRAY2BGR)
 
-        c7_index_to_pop = []
-        for i in range(len(c7_coords)):
-            if c7_coords[i][0] > c7_img_width - c7_x_offset:
-                c7_index_to_pop.append(i)
-        for i in range(len(c7_index_to_pop)):
-            c7_coords.pop(c7_index_to_pop[i] - i)
+        c2_bottom_points = find_c2_bottom_points(c2_edges)
+        c7_top_points = find_c7_top_points(c7_edges)
 
-        edges = cv2.cvtColor(c2_edges, cv2.COLOR_GRAY2BGR)
-
-        c2_bottom_points = find_c2_bottom_points()
-        c7_top_points = find_c7_top_points()
-
-        cv2.circle(edges, (c2_bottom_points[0][0], c2_bottom_points[0][1]), 3, (0, 0, 255), -1)
-        cv2.circle(edges, (c2_bottom_points[1][0], c2_bottom_points[1][1]), 3, (0, 0, 255), -1)
-        # cv2.circle(edges, (c7_top_points[0][0], c7_top_points[0][1]), 3, (0, 0, 255), -1)
-        # cv2.circle(edges, (c7_top_points[1][0], c7_top_points[1][1]), 3, (0, 0, 255), -1)
+        cv2.circle(c2_edges, (c2_bottom_points[0][0], c2_bottom_points[0][1]), 3, (0, 0, 255), -1)
+        cv2.circle(c2_edges, (c2_bottom_points[1][0], c2_bottom_points[1][1]), 3, (0, 0, 255), -1)
+        cv2.circle(c7_edges, (c7_top_points[0][0], c7_top_points[0][1]), 3, (0, 0, 255), -1)
+        cv2.circle(c7_edges, (c7_top_points[1][0], c7_top_points[1][1]), 3, (0, 0, 255), -1)
 
         final_c2_coords = coords_translate(c2_bottom_points, img_number, "c2")
         final_c7_coords = coords_translate(c7_top_points, img_number, "c7")
@@ -247,6 +261,8 @@ if __name__ == "__main__":
         print(f"{angle} {angle_degree}")
         
         
-        cv2.imshow("img", edges)
+        cv2.imshow("original", c7_img)
+        cv2.imshow("c2", c2_edges)
+        cv2.imshow("c7", c7_edges)
         cv2.imshow("line", line_img)
         cv2.waitKey(0)
